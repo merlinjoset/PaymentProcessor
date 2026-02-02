1c572aad4326290e0a1c6adfc4d8c2f5c6517272
MerlinJose89 <t.merlin72@gmail.com>
2026-02-02 13:32:50 +0400

    Update User

--- src/Assessment.Application/Dtos/Auth/CreateUserRequestDto.cs
+++ src/Assessment.Application/Dtos/Auth/CreateUserRequestDto.cs
@@ -5,13 +5,13 @@ namespace Assessment.Application.Dtos.Auth;
 public class CreateUserRequestDto
 {
     [Required, EmailAddress]
-    public string Email { get; set; } = default!;
+    public string? Email { get; set; }
 
     [Required, MinLength(3), MaxLength(50)]
-    public string UserName { get; set; } = default!;
+    public string? UserName { get; set; }
 
     [Required, MinLength(6), MaxLength(100)]
-    public string Password { get; set; } = default!;
+    public string? Password { get; set; }
 
     public bool IsAdmin { get; set; } = false;
 }
